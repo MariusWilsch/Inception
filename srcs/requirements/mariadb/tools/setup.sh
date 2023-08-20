82469bd95f4945b47cbad672cdd762db862645fb
Marius Santiago Wilsch <84270258+VerdantStartup@users.noreply.github.com>
2023-08-20 12:44:21 +0200

    Finished Inception - eval sheet checked - rdy to eval

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -2,8 +2,7 @@
 
 service mariadb start
 
-sleep 10
-if [ ! -f /var/lib/mysql/ibdata1 ]; then
+sleep 5
 
 mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
@@ -12,8 +11,6 @@ mariadb -e "FLUSH PRIVILEGES;"
 mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
 
 sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-fi
 
 kill $(cat /var/run/mysqld/mysqld.pid)
 
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
 echo "Sleeping for 20 seconds to wait for mariadb to configure..."
-sleep 15
+sleep 10
 
-if ! command -v wp &> /dev/null; then 
 # install wp-cli
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-
 # make it executable
 chmod +x wp-cli.phar 
-
 # move it into path
 mv wp-cli.phar /usr/local/bin/wp
-else
-	echo "wp-cli already installed"
-fi
 
 # ... 
 cd /var/www/html/
 
+# -f checks if file exists and is a regular file
 if [ ! -f wp-config.php ]; then
+	echo "wp-config.php not found, installing & configuring wordpress..."
 # download wordpress
 wp core download --allow-root
 
@@ -31,12 +27,12 @@ wp core install --url=$DOMAIN/ --title=title --admin_user=$ADMIN_USERNAME --admi
 
 # create user
 wp user create $USER_USERNAME $USER_EMAIL --role=$USER_ROLE --user_pass=$USER_PASSWORD --allow-root
-
+else
+	echo "Wordpress already installted & configured, skipping ..."
 fi
 
 # to connect php-fpm with nginx
 sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
-
 
 mkdir /run/php
 
